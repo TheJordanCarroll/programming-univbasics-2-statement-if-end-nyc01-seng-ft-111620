9ddfc2f6f10c01f6ccb2216a307311e396bdbd39
Jordan Carroll <jordancarroll1994@gmail.com>
2020-10-25 02:27:57 +0000

    Automatically backed up by Learn

--- spec/if_else_end_spec.rb
+++ spec/if_else_end_spec.rb
@@ -16,3 +16,11 @@ describe 'lib/if_else_end.rb' do
     expect{ load file }.to output(/Odd!/).to_stdout
   end
 end
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
